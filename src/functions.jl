b7a4978b8c3705233ddebf3aabf5e04395995f01
Benjamin Vittrant <benjamin.vittrant@withings.com>
2023-03-07 13:12:24 +0100

    update test doc for greetings

--- src/functions.jl
+++ src/functions.jl
@@ -7,7 +7,23 @@ function greet_your_package_name()
     println("Hello JulCom!")
 end
 
+###############################################################################
+
+"""
+    greetings()
+
+Simply say hell to you my friend :)
+
+# Arguments
+
+# Examples
+```julia-repl
+julia> JulCom.greetings()
+Greetings friend!
+```
+"""
 function greetings()
     println("Greetings friend!")
 end
+
 ###############################################################################
